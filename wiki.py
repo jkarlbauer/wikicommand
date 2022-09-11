5f7392ad1a107d3dab92dfc609d761c107c972eb
Julian Karlbauer <julian.karlbauer@audi.de>
2022-09-11 09:45:02 +0200

    added text summarization

--- wiki.py
+++ wiki.py
@@ -1,5 +1,7 @@
 import wikipediaapi
 import sys
+import gensim
+from gensim.summarization import summarize
 
 def main():
 	wiki = wikipediaapi.Wikipedia('en')
@@ -11,13 +13,17 @@ def main():
 
 	summary = page.summary.splitlines()
 
-	print_sparse(summary)
+	summarize_text_by_ratio(page.summary)
+
 
 def print_sparse(str_in):
 	for line in str_in:
 		print(line)
 		print('\n')
 
+def summarize_text_by_ratio(text):
+	summary_by_ratio = summarize(text,ratio=0.5)
+	print(summary_by_ratio)
 
 if __name__=="__main__":
 	main()
