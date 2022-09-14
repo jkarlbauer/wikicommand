3b132ba051145f2b6093347610f034616ce60920
Julian Karlbauer <julian.karlbauer@gmx.de>
2022-09-14 19:26:44 +0200

    working on adding ML

--- wiki.py
+++ wiki.py
@@ -2,11 +2,7 @@
 
 import wikipediaapi
 import sys
-import spacy
-from spacy.lang.en.stop_words import STOP_WORDS
-from string import punctuation
-from collections import Counter
-from heapq import nlargest
+from summarizer import TransformerSummarizer
 
 def main():
 	wiki = wikipediaapi.Wikipedia('en')
@@ -19,40 +15,12 @@ def main():
 	summary = page.summary.splitlines()
 	print("\n" + "SUMMARAY:" + "\n")
 	print(summary)
-	print("\n" + "COMPRESSED ARTICLE: " + "\n")
-	print(summarize(page.text, 0.1))
-    
-def summarize(txt, percentage):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(txt)
-    keyword = []
-    stopwords = list(STOP_WORDS)
-    pos_tag = ['PROPN', 'ADJ', 'NOUN', 'VERB']
-    for token in doc:
-        if(token.text in stopwords or token.text in punctuation):
-            continue
-        if(token.pos_ in pos_tag):
-            keyword.append(token.text)
-        
-    freq_word = Counter(keyword)
-    max_freq = Counter(keyword).most_common(1)[0][1]
-    for word in freq_word.keys():
-        freq_word[word] = (freq_word[word]/max_freq)
-    
-    sent_strength={}
-    for sent in doc.sents:
-        for word in sent:
-            if word.text in freq_word.keys():
-                if sent in sent_strength.keys():
-                    sent_strength[sent]+=freq_word[word.text]
-                else:
-                    sent_strength[sent]=freq_word[word.text]
+	print("\n" + "COMPRESSED ARTICLE: " + "\n" + summarize_text(page.text))
 
-    summarized_sentences = nlargest(3, sent_strength, key=sent_strength.get)
-    final_sentences = [ w.text for w in summarized_sentences ]
-    summary = ' '.join(final_sentences)
-    return summary
-
+def summarize_text(body):
+    model = TransformerSummarizer(transformer_type="XLNet",transformer_model_key="xlnet-base-cased")
+    full = ''.join(model(body, min_length=60))
+    return full
 
 if __name__=="__main__":
 	main()
